fcbbad43daf20100b4c984622f80d9b54186dbf4
Maksim Fesenko <drresist@gmail.com>
2022-04-15 15:00:27 +0300

    Change init message + setup release Java version to 17

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -3,11 +3,4 @@
  */
 package hexlet.code;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-
-    }
-}
